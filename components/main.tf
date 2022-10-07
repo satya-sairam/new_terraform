4af6afd5d8a1881872431cd4357ec2550976184b
satya <satyasairam144@gmail.com>
2022-10-07 18:10:12 +0530

    created ec2 for frontend and cart

---
+++ components/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "web" {
+  count          = length(var.components)
+  ami            = "ami-0bb6af715826253bf"
+  instance_type  = "t2.micro"
+
+  tags           = {
+    Name         = element(var.components,count.index )
+  }
+}
